ebbef9a71b7d56867c128d3987ea021662590505
Luciano Bessegato <lfrbessegato@gmail.com>
2019-10-16 23:19:52 -0300

    Atualização exercício 114

---
+++ exercicios/ex114.py
@@ -0,0 +1,11 @@
+print('Exercício: 114 - O Site está acessível?')
+import urllib
+import urllib.request
+
+try:
+    site = urllib.request.urlopen('http://www.pudim.com.br')
+except urllib.error.URLError:
+    print('O site Pudim não está acessível no momento.')
+else:
+    print('Consegui acessar o site Pudim com sucesso')
+    print(site.read())

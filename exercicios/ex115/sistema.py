a003f362ba95787e90ffc7f2d1ace466032c943a
Luciano Bessegato <lfrbessegato@gmail.com>
2019-11-16 21:00:05 -0300

    Atualizacao exercicio 115 - menu em Python

---
+++ exercicios/ex115/sistema.py
@@ -0,0 +1,27 @@
+from ex115.lib.interface import *
+from ex115.lib.arquivo import *
+from time import sleep
+
+arq = 'cursoemvideo.txt'
+
+if not arquivoExiste(arq):
+   criarArquivo(arq)
+
+print('Exercício 115 : Criando menu em Python')
+while True:
+    resposta = menu(['Ver pessoas cadastradas', 'Cadastrar nova Pessoa', 'Sair do Sistema'])
+    if resposta == 1:
+        # Opção de listar o conteúdo de um arquivo!
+        lerArquivo(arq)
+    elif resposta == 2:
+        # Opção de cadastrar uma nova pessoa
+        cabeçalho('NOVO CADASTRO')
+        nome = str(input('Nome: '))
+        idade = leiaInt('Idade: ')
+        cadastrar(arq, nome, idade)
+    elif resposta == 3:
+        cabeçalho('Saindo do Sistema... Até logo!')
+        break
+    else:
+        print('\033[31mERRO! Digite uma opção válida!\033[m')
+    sleep(2)
